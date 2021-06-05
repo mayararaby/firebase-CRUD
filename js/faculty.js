d08541c616abd8a8b2aba7818384d8bf95a9d509
mayararaby <mayararaby246@gmail.com>
2021-06-05 11:08:45 -0700

    CRUD opration for student using firebase and faculty

---
+++ js/faculty.js
@@ -0,0 +1,54 @@
+$(function() {
+    GetAllFaculties();
+})
+
+function GetAllFaculties() {
+    $.ajax({
+        url: "https://education-cc687-default-rtdb.firebaseio.com/faculty.json",
+        method: "GET",
+        dataType: "JSON",
+        success: function(data) {
+            $("#AllFaculties>tbody").empty();
+            for (var item in data) {
+                var row = `<tr>
+                        <td>${data[item].name}</td>
+                        <td>${data[item].address}</td>
+
+                    </tr>`;
+                $("#AllFaculties>tbody").append(row);
+            }
+            console.log(data)
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
+
+}
+
+function AddFaculty() {
+    var name = $("#facultyName").val();
+    var address = $("#facultyAddress").val();
+    var data = { name: name, address: address };
+
+    $.ajax({
+        url: "https://education-cc687-default-rtdb.firebaseio.com/faculty.json",
+        method: "POST",
+        contentType: "application/json",
+        dataType: "JSON",
+        data: JSON.stringify(data),
+        success: function(data) {
+            console.log(data);
+            GetAllFaculties();
+            Clear();
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+function Clear() {
+    $("#facultyName").val("");
+    $("#facultyAddress").val("");
+}
---
+++ js/student.js
@@ -0,0 +1,93 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBHXPPUAAQeKLbWvk8giV_oBKuLIIXwAe8",
+    authDomain: "education-cc687.firebaseapp.com",
+    projectId: "education-cc687",
+    storageBucket: "education-cc687.appspot.com",
+    messagingSenderId: "187289243283",
+    appId: "1:187289243283:web:b4ab9f74ad0d9a114fc766",
+    measurementId: "G-GZN4NCE4HS"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+database.ref('/faculty').on('value', (snapshot) => {
+    availableFaculties(snapshot.val())
+})
+
+function availableFaculties(data) {
+    $("#availableFaculties").empty();
+    for (var item in data) {
+        var row = `<option>${data[item].name}</option>`;
+        $("#availableFaculties").append(row);
+    }
+
+}
+
+database.ref('/student').on('value', (snapshot) => {
+    displayStudents(snapshot.val())
+})
+
+
+function displayStudents(data) {
+    $("#AllStudents>tbody").empty();
+    for (var item in data) {
+        var row = `<tr>
+                            <td>${data[item].firstName}</td>
+                            <td>${data[item].lastName}</td>
+                            <td>${(data[item].studentAge)}</td>
+                            <td>${(data[item].studentFaculty)}</td>
+                            <td>
+                                <button type="button" class="btn btn-warning" onclick="GetStudentByID('${item}')">Update</button>
+                                <button type="button" class="btn btn-danger" onclick="DeleteStudent('${item}')">Delete</button>
+                            </td>
+                        </tr>`;
+        $("#AllStudents>tbody").append(row);
+    }
+}
+
+function GetStudentByID(studentID) {
+    database.ref().child("student").child(studentID).get().then((snapshot) => {
+        var data = snapshot.val();
+        $("#studentID").val(studentID);
+        $("#firstName").val(data.firstName);
+        $("#lastName").val(data.lastName);
+        $("#studentAge").val(data.studentAge);
+        $("#availableFaculties").val(data.studentFaculty);
+    })
+}
+
+function AddStudent() {
+    var id = $("#studentID").val();
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var studentAge = $("#studentAge").val();
+    var studentFaculty = $("#availableFaculties").val();
+    var data = { firstName: firstName, lastName: lastName, studentAge: studentAge, studentFaculty: studentFaculty };
+    console.log(data);
+    if (id == "") {
+        database.ref().child('student').push(data);
+    } else {
+        var updates = {};
+        updates["/student/" + id] = data;
+        database.ref().update(updates);
+    }
+
+    Clear();
+}
+
+function Clear() {
+    $("#studentID").val("");
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#studentAge").val("");
+    $("#availableFaculties").val("");
+}
+
+function DeleteStudent(studentID) {
+    var sure = confirm("Are tou sure?");
+    if (sure) {
+        var updates = {};
+        updates["/student/" + studentID] = null;
+        database.ref().update(updates);
+    }
+}
